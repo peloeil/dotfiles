7f92f66359971dde4c2f238147b0df2dfc8b1505
Shun Ota <peloeil20@gmail.com>
2025-08-10 21:05:53 +0900

    feat: dpp.vim を使ってプラグインを管理

---
+++ config.ts
@@ -0,0 +1,70 @@
+import { type Dpp } from "jsr:@shougo/dpp-vim@4.5.0/dpp";
+import {
+  type ContextBuilder,
+  type Plugin,
+} from "jsr:@shougo/dpp-vim@4.5.0/types";
+import {
+  BaseConfig,
+  type ConfigArguments,
+  type ConfigReturn,
+} from "jsr:@shougo/dpp-vim@4.5.0/config";
+import { type Toml } from "jsr:@shougo/dpp-ext-toml";
+import { type Denops } from "jsr:@denops/core@7.0.1";
+import * as fn from "jsr:@denops/std@7.6.0/function";
+import { expandGlobSync } from "jsr:@std/fs@1.0.19";
+
+
+function gatherCheckFiles(path: string, glob: string): string[] {
+  const checkFiles: string[] = [];
+  for (const file of expandGlobSync(glob, { root: path, globstar: true })) {
+    checkFiles.push(file.path);
+  }
+  return checkFiles;
+}
+
+export class Config extends BaseConfig {
+  override async config(args: ConfigArguments): Promise<ConfigReturn> {
+    // dpp-protocol-git, dpp-ext-installer の設定
+    args.contextBuilder.setGlobal({
+      protocols: ["git"],
+      protocolParams: {
+        git: {
+          enablePartialClone: true,
+        },
+      },
+    });
+
+    const [context, options] = await args.contextBuilder.get(args.denops);
+    const configDir = await fn.expand(args.denops, "~/.config/nvim");
+
+    // toml の読み込み
+    const tomls: Toml[] = [];
+    tomls.push(
+      await args.dpp.extAction(
+        args.denops,
+        context,
+        options,
+        "toml",
+        "load",
+        {
+          path: configDir + "/dpp.toml",
+          options: {
+            lazy: false,
+          },
+        },
+      ) as Toml,
+    );
+
+    const plugins: Plugin[] = [];
+    for (const toml of tomls) {
+      for (const plugin of toml.plugins) {
+        plugins.push(plugin);
+      }
+    }
+
+    return {
+      checkFiles: gatherCheckFiles(configDir, "**/*.@(ts|toml|lua)"),
+      plugins: plugins,
+    };
+  }
+}
---
+++ init.lua
@@ -0,0 +1,93 @@
+if vim.loader then
+    vim.loader.enable()
+end
+vim.opt.number = true
+vim.opt.wrap = false
+
+vim.env.XDG_CONFIG_HOME = vim.fs.joinpath(vim.env.HOME, ".config")
+vim.env.XDG_CACHE_HOME = vim.fs.joinpath(vim.env.HOME, ".cache")
+
+local function install_plugin(repo_name)
+    local dpp_repos = vim.fs.joinpath(vim.env.XDG_CACHE_HOME, "dpp", "repos")
+    local url = "https://github.com/" .. repo_name
+    local repo_dir = vim.fs.joinpath(dpp_repos, repo_name)
+
+    if vim.fn.isdirectory(repo_dir) ~= 1 then
+        os.execute("git clone " .. url .. " " .. repo_dir)
+    end
+    vim.opt.runtimepath:prepend(repo_dir)
+end
+
+local function all_config_files()
+    local config_dir = vim.fs.joinpath(vim.env.XDG_CONFIG_HOME, "nvim")
+    local globs = {
+        "**/*.lua",
+        "**/*.ts",
+        "**/*.toml",
+    }
+    local files = {}
+    for _, glob in pairs(globs) do
+        table.insert(files, vim.fn.globpath(config_dir, glob, true, true))
+    end
+    return vim.iter(files):flatten():totable()
+end
+
+local function dpp_init()
+    install_plugin("Shougo/dpp-protocol-git")
+    install_plugin("Shougo/dpp-ext-toml")
+    install_plugin("Shougo/dpp-ext-lazy")
+    install_plugin("Shougo/dpp-ext-installer")
+    install_plugin("Shougo/dpp.vim")
+    install_plugin("vim-denops/denops.vim")
+end
+
+local function dpp_load()
+    local dpp_base = vim.fs.joinpath(vim.env.XDG_CACHE_HOME, "dpp")
+    local dpp_config = vim.fs.joinpath(vim.env.XDG_CONFIG_HOME, "nvim", "config.ts")
+    local dpp_autocmds = vim.api.nvim_create_augroup("__dpp_autocmds", { clear = true })
+
+    if vim.fn.isdirectory(dpp_base) ~= 1 then
+        os.execute("mkdir -p " .. dpp_base)
+    end
+
+    local dpp = require("dpp")
+    vim.api.nvim_create_autocmd("User", {
+        pattern = "Dpp:makeStatePost",
+        group = dpp_autocmds,
+        callback = function()
+            vim.notify("dpp make_state() is done", vim.log.levels.INFO)
+        end
+    })
+    if dpp.load_state(dpp_base) then
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "Dpp:makeStatePost",
+            group = dpp_autocmds,
+            once = true,
+            nested = true,
+            callback = function()
+                dpp.load_state(dpp_base)
+                vim.api.nvim_create_autocmd("User", {
+                    pattern = "Dpp:makeStatePost",
+                    group = dpp_autocmds,
+                    callback = function()
+                        vim.cmd.quit({ bang = true })
+                    end
+                })
+            end
+        })
+        vim.fn["denops#server#wait_async"](function()
+            dpp.make_state(dpp_base, dpp_config)
+        end)
+    end
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = all_config_files(),
+        group = dpp_autocmds,
+        callback = function()
+            vim.notify("dpp check_files() is run", vim.log.levels.INFO)
+            dpp.check_files()
+        end
+    })
+end
+
+dpp_init()
+dpp_load()

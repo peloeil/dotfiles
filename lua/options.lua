568907ff5191e3feb0e336963b07811563bc3fb0
Shun Ota <peloeil20@gmail.com>
2025-08-29 17:32:51 +0900

    chore: option の設定位置を移動

---
+++ lua/options.lua
@@ -0,0 +1,47 @@
+-- 検索
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- インデント
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "|->", trail = "-" }
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    group = vim.api.nvim_create_augroup("__42tokyo_norm", { clear = true }),
+    pattern = vim.fn.expand("~") .. "/**/42tokyo/**/*",
+    callback = function(args)
+        vim.bo[args.buf].expandtab = false
+        vim.bo[args.buf].tabstop = 4
+    end
+})
+
+-- エンコード
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8"
+
+-- 透明度
+vim.opt.winblend = 20
+vim.opt.pumblend = 20
+vim.api.nvim_create_autocmd("ColorScheme", {
+    group = vim.api.nvim_create_augroup("__set_bg_opacity", { clear = true }),
+    callback = function()
+        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+    end
+})
+
+-- その他
+vim.opt.number = true
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 10
+vim.opt.cursorline = true
+vim.opt.showbreak = "> "
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.wrap = false
+vim.opt.wildignorecase = true
+vim.g.mapleader = " "

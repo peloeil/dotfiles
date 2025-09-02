bf256bea3e2eeec239502ad3b23d544d8e517c48
Shun Ota <peloeil20@gmail.com>
2025-09-02 12:50:06 +0900

    feat: ddu-ui-filer で filer のウィンドウが表示された

---
+++ hooks/ddu-ui-filer.lua
@@ -0,0 +1,19 @@
+-- lua_post_source {{{
+vim.fn["ddu#custom#patch_local"]("filer", {
+    ui = "filer",
+    uiParams = {
+        filer = {
+            winWidth = vim.o.columns * 0.2,
+            split = "vertical",
+            splitDirection = "topleft",
+        },
+    },
+})
+local opts = { noremap = true, silent = true }
+vim.keymap.set(
+    "n",
+    "<C-n>",
+    [[<cmd>call ddu#start(#{name:"filer"})<cr>]],
+    opts
+)
+-- }}}

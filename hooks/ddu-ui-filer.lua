fd3e52626e91319c2a883f4199548f74c9ad378a
Shun Ota <peloeil20@gmail.com>
2025-09-02 18:20:12 +0900

    feat: ddu-ui-filer がまともに使えるようになった

--- hooks/ddu-ui-filer.lua
+++ hooks/ddu-ui-filer.lua
@@ -1,3 +1,17 @@
+-- lua_add {{{
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "ddu-filer",
+    group = vim.api.nvim_create_augroup("__ddu_filer", { clear = true }),
+    callback = function()
+        local opts = { noremap = true, silent = true, buffer = true }
+        vim.keymap.set("n", "q", [[<cmd>call ddu#ui#do_action("quit", {}, "filer")<cr>]], opts)
+        vim.keymap.set("n", "o", [[<cmd>call ddu#ui#do_action("expandItem", #{ mode: "toggle" }, "filer")<cr>]], opts)
+        vim.keymap.set("n", "<cr>", [[<cmd>call ddu#ui#do_action("itemAction", {}, "filer")<cr>]], opts)
+        vim.keymap.set("n", "<space>", [[<cmd>call ddu#ui#do_action("toggleSelectItem", {}, "filer")<cr>]], opts)
+    end
+})
+-- }}}
+
 -- lua_post_source {{{
 vim.fn["ddu#custom#patch_local"]("filer", {
     ui = "filer",

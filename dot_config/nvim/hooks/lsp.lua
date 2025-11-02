5e4f72897e628b64ab649f8552a9a513b9bfd0e0
Shun Ota <peloeil20@gmail.com>
2025-11-02 17:29:39 +0900

    feat(neovim): add ddu-source-lsp (not work)

---
+++ dot_config/nvim/hooks/lsp.lua
@@ -0,0 +1,44 @@
+-- lua_add {{{
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "ddu-ff",
+    group = vim.api.nvim_create_augroup("__ddu_lsp", { clear = true }),
+    callback = function()
+        local opts = { noremap = true, silent = true, buffer = true }
+        vim.keymap.set("n", "<cr>", [[<cmd>call ddu#ui#do_action("itemAction", #{name: "open"}, "lsp")<cr>]], opts)
+    end
+})
+-- }}}
+
+-- lua_post_source {{{
+vim.fn["ddu#custom#patch_local"]("lsp", {
+    -- ddu-ui-ff
+    ui = "ff",
+    uiParams = {
+        ff = {
+            immediateAction = "open",
+        },
+    },
+    -- ddu-source-lsp_definition
+    sources = {
+        name = { "lsp_definition" },
+    },
+    -- ddu-kind-lsp, ddu-kind-lsp_codeAction
+    kindOptions = {
+        lsp = {
+            defaultAction = "open",
+        },
+        lsp_codeAction = {
+            defaultAction = "apply"
+        },
+    },
+    sync = true,
+})
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set(
+    "n",
+    "<leader>l",
+    [[<cmd>call ddu#start(#{name:"lsp"})<cr>]],
+    opts
+)
+-- }}}

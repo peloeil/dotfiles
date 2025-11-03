84d197cccc53e8dfae62213f6f16c7b004a19383
Shun Ota <peloeil20@gmail.com>
2025-11-03 20:43:07 +0900

    feat(neovim): add heirline.nvim

---
+++ dot_config/nvim/hooks/heirline.lua
@@ -0,0 +1,107 @@
+-- lua_post_source {{{
+local conditions = require("heirline.conditions")
+local utils = require("heirline.utils")
+local colors = {
+    bright_bg = utils.get_highlight("Folded").bg,
+    bright_fg = utils.get_highlight("Folded").fg,
+    red = utils.get_highlight("DiagnosticError").fg,
+    dark_red = utils.get_highlight("DiffDelete").bg,
+    green = utils.get_highlight("String").fg,
+    blue = utils.get_highlight("Function").fg,
+    gray = utils.get_highlight("NonText").fg,
+    orange = utils.get_highlight("Constant").fg,
+    purple = utils.get_highlight("Statement").fg,
+    cyan = utils.get_highlight("Special").fg,
+    diag_warn = utils.get_highlight("DiagnosticWarn").fg,
+    diag_error = utils.get_highlight("DiagnosticError").fg,
+    diag_hint = utils.get_highlight("DiagnosticHint").fg,
+    diag_info = utils.get_highlight("DiagnosticInfo").fg,
+    git_del = utils.get_highlight("diffDeleted").fg,
+    git_add = utils.get_highlight("diffAdded").fg,
+    git_change = utils.get_highlight("diffChanged").fg,
+}
+
+require("heirline").load_colors(colors)
+
+local vimode = {
+    init = function(self)
+        self.mode = vim.fn.mode(1) -- :h mode()
+    end,
+    static = {
+        mode_names = {
+            n = "N",
+            no = "N?",
+            nov = "N?",
+            noV = "N?",
+            ["no\22"] = "N?",
+            niI = "Ni",
+            niR = "Nr",
+            niV = "Nv",
+            nt = "Nt",
+            v = "V",
+            vs = "Vs",
+            V = "V_",
+            Vs = "Vs",
+            ["\22"] = "^V",
+            ["\22s"] = "^V",
+            s = "S",
+            S = "S_",
+            ["\19"] = "^S",
+            i = "I",
+            ic = "Ic",
+            ix = "Ix",
+            R = "R",
+            Rc = "Rc",
+            Rx = "Rx",
+            Rv = "Rv",
+            Rvc = "Rv",
+            Rvx = "Rv",
+            c = "C",
+            cv = "Ex",
+            r = "...",
+            rm = "M",
+            ["r?"] = "?",
+            ["!"] = "!",
+            t = "T",
+        },
+        mode_colors = {
+            n = "red" ,
+            i = "green",
+            v = "cyan",
+            V =  "cyan",
+            ["\22"] =  "cyan",
+            c =  "orange",
+            s =  "purple",
+            S =  "purple",
+            ["\19"] =  "purple",
+            R =  "orange",
+            r =  "orange",
+            ["!"] =  "red",
+            t =  "red",
+        }
+    },
+    provider = function(self)
+        return " %2("..self.mode_names[self.mode].."%)"
+    end,
+    hl = function(self)
+        local mode = self.mode:sub(1, 1)
+        return { fg = self.mode_colors[mode], bold = true, }
+    end,
+    update = {
+        "ModeChanged",
+        pattern = "*:*",
+        callback = vim.schedule_wrap(function()
+            vim.cmd("redrawstatus")
+        end),
+    },
+}
+
+local align = { provider = "%=" }
+
+local statusline = {
+    vimode,
+    align
+}
+
+require("heirline").setup({ statusline = statusline })
+-- }}}

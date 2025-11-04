f0dfad3284ecb5ecbd22388aa33b506b3f4bfc4d
Shun Ota <peloeil20@gmail.com>
2025-11-05 00:34:40 +0900

    refactor(neovim): lsp config

---
+++ dot_config/nvim/hooks/lsp.lua
@@ -0,0 +1,29 @@
+-- lua_source {{{
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '󰋇',
+            [vim.diagnostic.severity.HINT] = '󰌵',
+        },
+    },
+})
+vim.lsp.config("*", {
+    capabilities = require("ddc_source_lsp").make_client_capabilities(),
+})
+
+local server_langs = {
+    lua_ls = { "lua" },
+    clangd = { "c", "cpp" },
+}
+for server, langs in pairs(server_langs) do
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = langs,
+        callback = function()
+            vim.lsp.enable(server)
+        end
+    })
+end
+-- }}}

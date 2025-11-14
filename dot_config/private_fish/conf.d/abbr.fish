1251c028116800ecb61833571aeb369e47fb6681
Shun Ota <peloeil20@gmail.com>
2025-11-14 21:33:56 +0900

    chore(fish): delete all config

--- dot_config/private_fish/conf.d/abbr.fish
+++
@@ -1,9 +0,0 @@
-function last_history_item
-    echo $history[1]
-end
-
-abbr --add !! --position anywhere --function last_history_item
-abbr --add co --command git commit
-abbr --add sw --command git switch
-abbr --add swc --command git "switch -c"
-abbr --add ls lsd

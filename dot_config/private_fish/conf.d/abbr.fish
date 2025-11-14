74d7f20f63a2c6bafb08847b11a845cd4f044260
Shun Ota <peloeil20@gmail.com>
2025-11-14 22:29:34 +0900

    feat(git): add git status abbrebiation

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -4,6 +4,7 @@ end
 
 abbr --add !! --position anywhere --function last_history_item
 abbr --add co --command git commit
+abbr --add st --command git status
 abbr --add sw --command git switch
 abbr --add swc --command git "switch -c"
 abbr --add ls lsd
